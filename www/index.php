0a822fa9586b26c186dbbab01cd166a2a1f64045
Merkulova Elizaveta <kurlyk013@gmail.com>
2026-05-14 01:33:20 +0200

    feat: style to insex.php

--- www/index.php
+++ www/index.php
@@ -6,24 +6,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style_index.css">
-    <title>Document</title>
+    <title>Data</title>
 </head>
-
-</html>
+<body>
 
 <?php if(isset($_SESSION['username'])): ?>
-    <p>Данные из сессии:</p>
+    <h3>Данные из сессии:</h3>
+    <div class="card">
     <ul>
         <li>Имя: <?= $_SESSION['username'] ?></li>
         <li>Email: <?= $_SESSION['email'] ?></li>
     </ul>
+</div>
 <?php else: ?>
     <p>Данных пока нет.</p>
 <?php endif; ?>
 
-<a href="form.html">Заполнить форму</a> |
-<a href="view.php">Посмотреть все данные</a> |
-<a href="cookies.php">Куки</a>
 
 <?php if(isset($_SESSION['errors'])): ?>
     <ul style="color:red;">
@@ -42,9 +40,19 @@
 <?php require_once 'UserInfo.php';
 $info = UserInfo::getInfo();
 
-echo "<h3>Информация о пользователе:</h3>";
-foreach ($info as $key => $val) {
+echo "<h3>Информация о пользователе:</h3>"; ?>
+<div class="card">
+<?php foreach ($info as $key => $val) {
     echo htmlspecialchars($key) . ': ' . htmlspecialchars($val) . '<br>';
 } ?>
+</div>
 
+<div class="nav">
+<a href="form.html">Заполнить форму</a> |
+<a href="view.php">Посмотреть все данные</a> |
+<a href="cookies.php">Куки</a>
+</div>  
+
+</body> 
+</html>
 
